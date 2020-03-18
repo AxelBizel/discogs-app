7e61d8bf3e3d7ccc0bc1e0e84ef2ed7f5790bb84
Axel Bizel <axel@maans.eu>
2020-03-18 11:51:00 +0100

    graph yearsAdded component

--- front/src/components/DashboardYearsAdded.js
+++ front/src/components/DashboardYearsAdded.js
@@ -1,29 +1,27 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
-import { Line } from "react-chartjs-2";
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { Line } from 'react-chartjs-2'
 
 const DashboardYearsAdded = yearsAdded => {
+  let yearsAddedArray = yearsAdded.yearsAdded.sort()
+  console.log(yearsAddedArray[0], typeof yearsAddedArray[0])
+  let graphLabels = [
+    //   new Date(yearsAddedArray[0].getYear())
+    ]
+  let graphValues = [0]
 
-    let yearsAddedArray = yearsAdded.yearsAdded.sort()
-    console.log(yearsAddedArray)
-    let graphLabels = []
-    let graphValues = []
-    let parsedYearsAdded = []
-
-    for (let i = 0 ; i < yearsAddedArray.length; i++){
-        graphLabels.push(yearsAddedArray[i]);
-        graphValues.push(i)
-    }
-
-    console.log(parsedYearsAdded)
+  for (let i = 0; i < yearsAddedArray.length; i++) {
+    graphLabels.push(yearsAddedArray[i])
+    graphValues.push(i)
+  }
 
 
-    //   let yearsArray = years.years.map(y => {
-//     return y ? y : 0;
-//   });
-//   let firstYear = yearsArray.findIndex((y, i) => y > 0 && i > 0);
-//   let graphLabels = yearsArray.map((y, i) => i).slice(firstYear);
+  //   let yearsArray = years.years.map(y => {
+  //     return y ? y : 0;
+  //   });
+  //   let firstYear = yearsArray.findIndex((y, i) => y > 0 && i > 0);
+  //   let graphLabels = yearsArray.map((y, i) => i).slice(firstYear);
 
   const data = {
     labels: graphLabels,
@@ -31,38 +29,56 @@ const DashboardYearsAdded = yearsAdded => {
       {
         fill: true,
         lineTension: 0.2,
-        backgroundColor: "rgba(75,192,192,0.4)",
-        borderColor: "rgba(75,192,192,1)",
-        borderCapStyle: "butt",
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
         borderDash: [],
         borderDashOffset: 0.0,
-        borderJoinStyle: "miter",
-        pointBorderColor: "rgba(75,192,192,1)",
-        pointBackgroundColor: "#fff",
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
         pointBorderWidth: 1,
         pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgba(75,192,192,1)",
-        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
         pointHoverBorderWidth: 2,
         pointRadius: 1,
         pointHitRadius: 10,
         data: graphValues,
-        label: "Nombre de disques par année de sortie"
+        label: "Nombre de disques dans la collection"
       }
     ]
-  };
+  }
+
+  const options = {
+    scales: {
+      xAxes: [
+        {
+          type: 'time',
+          time: {
+            unit: 'year'
+          }
+        }
+      ],
+      yAxes: [{
+        ticks: {
+            beginsAtZero: true,
+        }
+    }]
+    }
+  }
 
   return (
-    <div style={{ width: "100%" }}>
+    <div style={{ width: '100%' }}>
       {yearsAdded ? (
         <div>
           <h3>Répartition par date d'ajout dans la collection</h3>
-          <Line data={data} />
+          <Line data={data} legend={{ display: false }} options={options} />
         </div>
       ) : (
         <FontAwesomeIcon icon={faSpinner} spin />
       )}
     </div>
-  );
-};
-export default DashboardYearsAdded;
+  )
+}
+export default DashboardYearsAdded
